6627c3aff3acdfb6d217b36552ba4404e2d0e628
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-05-23 20:40:55 -0400

    first try

---
+++ src/views/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src="https://download.logo.wine/logo/Premier_League/Premier_League-Logo.wine.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                    The Premier League Blog
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+
+export default Navbar;
