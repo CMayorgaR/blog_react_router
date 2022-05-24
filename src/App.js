7fc9fb457130abecd39041bd1f20385be674b1c5
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-05-23 21:20:23 -0400

    Se añadió cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+//import { render } from "react-dom";
+//import { BrowserRouter, Routes, Route,} from "react-router-dom";
 import Navbar from './views/Navbar';
+import Cards from './views/Cards';
 
 
 const App = () => {
   return <>
     <Navbar />
+    <Cards />
   </>
 }
 
--- src/views/Navbar.js
+++ src/views/Navbar.js
@@ -1,12 +1,22 @@
 const Navbar = () => {
     return (
-        <nav className="navbar bg-light">
-            <div className="container-fluid">
+        <nav className="navbar container-fluid bg-dark d-flex justify-content-between">
+            <div className="flex-row">
                 <a className="navbar-brand" href="#">
-                    <img src="https://download.logo.wine/logo/Premier_League/Premier_League-Logo.wine.png" alt="" width="30" height="24" class="d-inline-block align-text-top" />
-                    The Premier League Blog
+                    <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                    El Blog de...
                 </a>
             </div>
+            <span className="flex-row">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                        Favoritos
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                        <li>Holi</li>
+                    </ul>
+                </div>
+            </span>
         </nav>
     )
 }
