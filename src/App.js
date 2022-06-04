70723b8f50e63787c3620a735dd2ca9b21fa3464
Camilo Mayorga Ríos <camilo@MacBook-Air-de-Camilo.local>
2022-06-04 15:08:54 -0400

    random imgs home

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; //Primero se importa lo siguiente:
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./views/Home";
 import Characters from "./views/Characters";
 import Lugares from "./views/Lugares";
 import Episodios from "./views/Episodios";
 
-//Creamos el componente principal que es donde van a confluir todas las rutas.
 const App = () => {
   return (
     <div>
-      <BrowserRouter //es un componente de RRD que nos permite crear la lógica de enrutamiento de c/u de nuestras vistas. Elemento Padre.
-      >
+      <BrowserRouter>
         <Navbar />
-        <Routes //siempre es un hijo de Browser Router. Es un componente que cumple la función de "switch". Va a contener todas las rutas. Es el componente que "decide" que componentes se verán cuando se acceda a una ruta X.
-        >
-          <Route //recibe 2 parámetros: path que es la ruta y element que es la vista (expresada como componente de React)
-            path="/home"
-            element={<Home />} //se pasa directamente el componente declarado en HOME.
-          />
-          <Route path="/characters" element= {<Characters />} />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/characters" element={<Characters />} />
           <Route path="/lugares" element={<Lugares />} />
           <Route path="/episodios" element={<Episodios />} />
         </Routes>
@@ -31,3 +25,8 @@ const App = () => {
 };
 
 export default App;
+
+//Creamos el componente principal que es donde van a confluir todas las rutas (en este caso App).
+//es un componente de RRD que nos permite crear la lógica de enrutamiento de c/u de nuestras vistas. Elemento Padre.
+//Routes siempre es un hijo de Browser Router. Es un componente que cumple la función de "switch". Va a contener todas las rutas. Es el componente que "decide" que componentes se verán cuando se acceda a una ruta X.
+//Route recibe 2 parámetros: path que es la ruta y element que es la vista (expresada como componente de React)
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,8 +1,27 @@
-const Home =() => {
-    return <img 
-    src="https://www.magicalquote.com/wp-content/uploads/2021/10/Everyone-can-change-their-nature-Morty.-Its-what-defines-our-species.-Look-at-Iron-Man.jpg" 
-    className= "img-fluid"
-    />
+import { useState, useEffect } from "react";
+
+const images = [
+  "http://quotesbae.com/wp-content/uploads/2018/03/rick-and-morty-quotes-16.jpg",
+  "https://www.magicalquote.com/wp-content/uploads/2021/10/Everyone-can-change-their-nature-Morty.-Its-what-defines-our-species.-Look-at-Iron-Man.jpg",
+  "https://i.pinimg.com/originals/b6/bc/e2/b6bce20b2befaedbfce69391f8e5e6ee.jpg",
+  "https://i.pinimg.com/originals/b6/bc/e2/b6bce20b2befaedbfce69391f8e5e6ee.jpg",
+  "https://i.pinimg.com/originals/38/cb/50/38cb50cf31dd4748c65fbea5865ad2bc.png",
+  "http://cdn.costumesupercenter.com/images/content/RICK-AND-MORTY-TEXT-GRAPHICS-02-1.jpg",
+  "https://static1.cbrimages.com/wordpress/wp-content/uploads/2020/04/Rick-and-Morty-Quote-11-Cropped.jpg?q=50&fit=crop&w=630&dpr=1.5",
+  "https://wallpaperaccess.com/full/3931789.png",
+  "https://i.pinimg.com/736x/81/01/52/8101524ea9f96401480f98702b104b4c.jpg",
+  "https://www.magicalquote.com/wp-content/uploads/2021/05/Youre-a-monster.-Youre-like-Hitler-but-even-Hitler-cared-about-Germany-or-something.jpg",
+];
+
+const Home = () => {
+  const [quote, setQuote] = useState();
+
+  useEffect(() => {
+    setQuote(Math.floor(Math.random() * images.length));
+  });
+  return <img 
+  src={images[quote]} 
+  className="img-fluid" />;
 };
 
 export default Home;
